78183e2c4e85507760f82c02e1b8af460d4f56c7
10241037-cloud <10241037@student.itk.ac.id>
2026-09-04 16:02:01 +0800

    feat: tambah route /tentang

--- lms-team-4/routes/web.php
+++ lms-team-4/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/tentang', function () {
+   return 'Kelompok 4: Feniria, Fitriyansyah Wicaksonoaji, Hamzah Wiranata, Jeshua Austin Daceka';
+   });
